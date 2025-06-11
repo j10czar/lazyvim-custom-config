7c26822ff1b40f2086fb6d0cf59c6dffa4f6c659
Tenczar <jason.tenczar@cn9wrll67v.wdw.disney.com>
2025-06-11 11:53:48 -0400

    new changes to default ignore diagnositcs

--- plugins/lsp-config.lua
+++ plugins/lsp-config.lua
@@ -1,14 +1,11 @@
 -- for this start with the lazy extra for the python language, this will tone the errors down
+-- you can also do <ud> to toggle these
 
 return {
   -- change nvim-lspconfig options
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
-      ruff_lsp = {
-        mason = false, -- Prevent Mason from installing ruff-lsp
-      },
-      -- https://github.com/microsoft/pyright/discussions/5852#discussioncomment-6874502
       pyright = {
         capabilities = {
           textDocument = {
